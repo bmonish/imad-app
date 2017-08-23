877d4fb750b9bde4ca0dc64510b8b2fbdc770b4d
bmonish <31185862+bmonish@users.noreply.github.com>
2017-08-23 20:45:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content ={
+  title: 'Article One | Monish Baskar',  
+  heading: 'Article One',
+  date: 'Aug 23,2017'
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
